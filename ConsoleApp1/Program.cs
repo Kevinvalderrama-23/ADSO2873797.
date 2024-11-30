caebb7832814890a2a7120ed2a888d0f50088961
Kevin <valderramak05@gmail.com>
2024-11-29 20:06:04 -0500

    SUMA Y RESTA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,20 +11,62 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
            
-            Console.WriteLine("digite un numero");
-            int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("digite un numero");
+            //int num1 = int.Parse(Console.ReadLine());
 
-            if (num1 % 2 == 0)
-            {
-                Console.WriteLine($"El numero {num1} es par ");
+            //if (num1 % 2 == 0)
+            //{
+            //    Console.WriteLine($"El numero {num1} es par ");
                 
-            }
-            else
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"El numero {num1} es impar");
+            //}
+
+            //Console.ReadKey();
+
+
+            Console.WriteLine(" A continuación vamos a crear un algoritmo de suma o resta, digite 1 si es suma, o 2 si es resta ");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero == 1)
             {
-                Console.WriteLine($"El numero {num1} es impar");
+                Console.WriteLine("Es una suma");
+                Console.WriteLine("Digite el primer numero");
+
+                int num3 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite el segundo numero");
+
+                int num4 = int.Parse(Console.ReadLine());
+
+
+                int resultado = num3 + num4;
+
+                Console.WriteLine($"El resultado es {resultado}");
+
             }
+            if (numero == 2)
+            {
+                Console.WriteLine("Es una resta");
+                Console.WriteLine("Digite el primer numero");
+
+                int num3 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite el segundo numero");
+
+                int num4 = int.Parse(Console.ReadLine());
+
+                int resultado = num3 - num4;
+
+                Console.WriteLine($"El resultado es {resultado}");
+            }
+
 
             Console.ReadKey();
+
 
         }
     }
