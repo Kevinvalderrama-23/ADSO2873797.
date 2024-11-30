f0f6bb77979a5345bf6dd8828734e3cfc2e18ea3
Kevin <valderramak05@gmail.com>
2024-11-29 19:49:44 -0500

    creamos la segunda rama

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+           
+            Console.WriteLine("digite un numero");
+            int num1 = int.Parse(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine($"El numero {num1} es par ");
+                
+            }
+            else
+            {
+                Console.WriteLine($"El numero {num1} es impar");
+            }
+
+            Console.ReadKey();
+
         }
     }
 }
